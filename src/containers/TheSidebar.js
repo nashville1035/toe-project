98d3d42b72cb4bbdaf55a5e6260f43657ec811be
omarx <omarpeart100@gmail.com>
2021-08-15 22:25:30 -0500

    Dash Board First Commit

---
+++ src/containers/TheSidebar.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import {
+  CCreateElement,
+  CSidebar,
+  CSidebarBrand,
+  CSidebarNav,
+  CSidebarNavDivider,
+  CSidebarNavTitle,
+  CSidebarMinimizer,
+  CSidebarNavDropdown,
+  CSidebarNavItem,
+} from '@coreui/react'
+
+import CIcon from '@coreui/icons-react'
+
+// sidebar nav config
+import navigation from './_nav'
+
+const TheSidebar = () => {
+  const dispatch = useDispatch()
+  const show = useSelector(state => state.sidebarShow)
+
+  return (
+    <CSidebar
+      show={show}
+      onShowChange={(val) => dispatch({type: 'set', sidebarShow: val })}
+    >
+      <CSidebarBrand className="d-md-down-none" to="/">
+        <img 
+          className="c-sidebar-brand-full"
+          src="media/thermal_eyes.jpeg"
+          height={70}
+        /> Thermal Ocean Eye
+        <img 
+          className="c-sidebar-brand-minimized"
+          name="media/thermal_eyes.jpeg"
+          height={70}
+        />
+      </CSidebarBrand>
+      <CSidebarNav>
+
+        <CCreateElement
+          items={navigation}
+          components={{
+            CSidebarNavDivider,
+            CSidebarNavDropdown,
+            CSidebarNavItem,
+            CSidebarNavTitle
+          }}
+        />
+      </CSidebarNav>
+      <CSidebarMinimizer className="c-d-md-down-none"/>
+    </CSidebar>
+  )
+}
+
+export default React.memo(TheSidebar)
