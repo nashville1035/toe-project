9e02472911f3033eee7d1ad5efa9bbbc3e42039c
MLK <mlk.cerberus@outlook.com>
2021-08-20 17:57:44 -0500

    feat: update add temperature

--- pages/temperature/manage.vue
+++ pages/temperature/manage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="con flex flex-col space-y-16">
-    <div class="flex items-center space-x-4">
+    <!-- <div class="flex items-center space-x-4">
       <h1 class="text-2xl">Manage Temperature</h1>
 
       <Dropdown
@@ -20,9 +20,169 @@
         :disabled="!selectedLocation.id"
         :options="years"
       />
-    </div>
+    </div> -->
+    <Toolbar>
+      <template #left>
+        <h1 class="text-3xl">Manage Temperatures</h1>
+      </template>
 
-    <template v-if="state === 'selected'">
+      <template #right>
+        <div class="flex items-center space-x-4">
+          <Dropdown
+            v-model="selectedLocation"
+            class="w-60"
+            option-label="name"
+            placeholder="Location"
+            :options="locationsByName"
+          />
+
+          <template v-if="selectedLocation.id">
+            <Button
+              v-if="state === 'default'"
+              label="New"
+              icon="mdi mdi-plus"
+              @click="state = 'add-temperature'"
+            />
+
+            <Button
+              v-else
+              class="!bg-secondary-dark !text-white"
+              icon="mdi mdi-close"
+              @click="closeAddTemperature"
+            />
+          </template>
+        </div>
+      </template>
+    </Toolbar>
+
+    <Card v-if="state === 'add-temperature'">
+      <template #title>
+        <h1 class="text-2xl">
+          Add Temperature for <em>{{ selectedLocation.name }}</em>
+        </h1>
+      </template>
+
+      <template #content>
+        <form
+          class="flex flex-col space-y-8 w-80"
+          @submit.prevent="saveTemperature"
+        >
+          <div class="space-y-2">
+            <Dropdown
+              v-model="addForm.year"
+              class="w-full"
+              option-label="name"
+              option-value="value"
+              placeholder="Year"
+              :options="years"
+            />
+
+            <p
+              v-if="$v.addForm.year.$error && !$v.addForm.year.required"
+              class="error"
+            >
+              You must provide a year
+            </p>
+          </div>
+
+          <div class="space-y-2">
+            <Dropdown
+              v-model="addForm.month"
+              class="w-full"
+              option-label="name"
+              option-value="value"
+              placeholder="Select a month"
+              :options="months"
+            />
+
+            <p
+              v-if="$v.addForm.month.$error && !$v.addForm.month.required"
+              class="error"
+            >
+              You must provide a month
+            </p>
+          </div>
+
+          <div class="space-y-2">
+            <span class="p-float-label">
+              <InputNumber
+                id="min"
+                v-model.lazy="addForm.min"
+                class="w-full"
+                mode="decimal"
+                :min-fraction-digits="3"
+                :max-fraction-digits="3"
+                @blur="$v.addForm.min.$touch()"
+              />
+
+              <label for="min">Minimum</label>
+            </span>
+
+            <p
+              v-if="$v.addForm.min.$error && !$v.addForm.min.required"
+              class="error"
+            >
+              You must provide a minimum temperature
+            </p>
+          </div>
+
+          <div class="space-y-2">
+            <span class="p-float-label">
+              <InputNumber
+                id="max"
+                v-model.lazy="addForm.max"
+                class="w-full"
+                mode="decimal"
+                :min-fraction-digits="3"
+                :max-fraction-digits="5"
+                @blur="$v.addForm.max.$touch()"
+              />
+
+              <label for="max">Maximum</label>
+            </span>
+
+            <p
+              v-if="$v.addForm.max.$error && !$v.addForm.max.required"
+              class="error"
+            >
+              You must provide a maximum temperature
+            </p>
+          </div>
+
+          <div class="space-y-2">
+            <span class="p-float-label">
+              <InputNumber
+                id="avg"
+                v-model.lazy="addForm.avg"
+                class="w-full"
+                mode="decimal"
+                :min-fraction-digits="3"
+                :max-fraction-digits="3"
+                @blur="$v.addForm.avg.$touch()"
+              />
+
+              <label for="avg">Average</label>
+            </span>
+
+            <p
+              v-if="$v.addForm.avg.$error && !$v.addForm.avg.required"
+              class="error"
+            >
+              You must provide an average temperature
+            </p>
+          </div>
+
+          <Button
+            :disabled="isSaving"
+            label="Add"
+            class="p-button-outlined p-button-sm"
+            type="submit"
+          />
+        </form>
+      </template>
+    </Card>
+
+    <!-- <template v-if="state === 'selected'">
       <Card>
         <template #title>
           <h1 class="text-2xl">
@@ -167,31 +327,49 @@
           </DataTable>
         </template>
       </Card>
-    </template>
+    </template> -->
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex'
+import { validationMixin } from 'vuelidate'
+import { required } from 'vuelidate/lib/validators'
 
 export default {
+  mixins: [validationMixin],
+
   data() {
     return {
-      avg: 0,
+      addForm: {
+        avg: 0,
+        max: 0,
+        min: 0,
+        month: '',
+        year: '',
+      },
       isLoadingTemperatures: false,
       isSaving: false,
       filters: {},
-      max: 0,
-      min: 0,
       selectedLocation: {},
-      selectedMonth: '',
-      selectedYear: '',
       state: 'default',
     }
   },
+
+  validations: {
+    addForm: {
+      avg: { required },
+      max: { required },
+      min: { required },
+      month: { required },
+      year: { required },
+    },
+  },
+
   async fetch() {
     await this.loadLocationsAsync()
   },
+
   computed: {
     ...mapGetters('temperature', ['locationsByName']),
 
@@ -206,7 +384,7 @@ export default {
     },
 
     years() {
-      const startYear = 1970
+      const startYear = 1900
 
       return Array.from(
         { length: 1 + this.currentYear - startYear },
@@ -264,30 +442,39 @@ export default {
 
     async saveTemperature() {
       if (!this.isSaving) {
-        try {
-          this.isSaving = true
-          const { avg, min, max } = this
-          const item = {
-            location: this.selectedLocation.id,
-            month: this.selectedMonth,
-            year: this.selectedYear,
-            avg,
-            min,
-            max,
+        this.$v.addForm.$touch()
+        if (!this.$v.addForm.$invalid) {
+          try {
+            this.isSaving = true
+            const { avg, min, max, month, year } = this.addForm
+            const location = this.selectedLocation.id
+            const item = {
+              avg,
+              location,
+              max,
+              min,
+              month,
+              year,
+            }
+
+            await this.addTemperatureAsync(item)
+            await this.loadTemperaturesAsync(location)
+
+            this.$toast.add({
+              severity: 'success',
+              summary: 'Temperature Saved',
+              detail: `A temperature has been saved for ${this.getMonthText(
+                month,
+              )} ${year}.`,
+              life: 3000,
+            })
+
+            this.resetAddForm()
+          } catch (err) {
+          } finally {
+            this.isSaving = false
           }
-          await this.addTemperatureAsync(item)
-          await this.loadTemperaturesAsync(this.selectedLocation.id)
-          this.$toast.add({
-            severity: 'success',
-            summary: 'Temperature Saved',
-            detail: `A temperature has been saved for ${this.getMonthText(
-              this.selectedMonth,
-            )} ${this.selectedYear}.`,
-            life: 3000,
-          })
-          this.resetAddTempForm()
-          this.isSaving = false
-        } catch (err) {}
+        }
       }
     },
 
@@ -297,11 +484,13 @@ export default {
       return this.$dateFns.format(dateMonth, 'LLLL')
     },
 
-    resetAddTempForm() {
-      this.avg = 0
-      this.max = 0
-      this.min = 0
-      this.selectedMonth = ''
+    resetAddForm() {
+      this.addForm.avg = 0
+      this.addForm.max = 0
+      this.addForm.min = 0
+      this.addForm.month = ''
+      this.addForm.year = ''
+      this.$v.addForm.$reset()
     },
 
     showDeleteConfirmation(item) {
@@ -326,6 +515,11 @@ export default {
           await this.loadTemperaturesAsync(this.selectedLocation.id)
         },
       })
+    },
+
+    closeAddTemperature() {
+      this.state = 'default'
+      this.resetAddForm()
     },
   },
 }
